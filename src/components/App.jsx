40a7555c13bce471274842d397b276e6be768e5e
GoStars <gostars@users.noreply.github.com>
2017-09-25 00:45:28 +0300

    Add components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
-import {Header} from './User.jsx'
+import {Header} from './Header.jsx'
+import {Orders} from './Orders.jsx'
+import {Group} from './Group.jsx'
 
 export class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            
+            users: []
         }
     }
 
     componentDidMount() {
-
+        fetch('/api')
+            .then(res => res.json())
+            .then(users => this.setState({users}))
     }
 
     componentWillUnmount() {
@@ -22,15 +26,16 @@ export class App extends React.Component {
     render() {
         return (
             <div className="container">
-                <Header />
-                <div>
-                    <p className="text-center">Hello, {this.props.name}!</p>
-                    <ul>
-                        <li>Order1</li>
-                        <li>Order2</li>
-                        <li>Order3</li>
-                    </ul>
-                </div>
+                <Header /> 
+                <p className="text-center">Hello, {this.props.name}!</p>
+                <hr />
+                <Orders />
+                <hr />
+                <Group />
+                <h3>Users</h3>
+                {this.state.users.map(user => 
+                    <div key={user.id}>{user.username}</div>
+                )}
             </div>
         )
     }
